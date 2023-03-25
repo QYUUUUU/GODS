72754417355a6f848c43d570e924811ea62bcb6c
Alexandre Choplin <achop@info-p-lp-18.iut.bx>
2023-03-25 17:53:48 +0100

    Modal reposition

--- Rolls/js/sidebar.js
+++ Rolls/js/sidebar.js
@@ -1,16 +1,14 @@
 const openOverlayBtn = document.getElementById("open-overlay-btn");
+const openOverlayBox = document.getElementById("rightside");
 const overlay = document.getElementById("overlay");
 const originalContent = document.getElementById("original-content");
-const redcross = document.getElementById("redcross");
 
-openOverlayBtn.addEventListener("click", () => {
+openOverlayBox.addEventListener("click", () => {
   overlay.classList.toggle("show");
   originalContent.classList.toggle("show");
-});
-
-
-
-redcross.addEventListener("click", ()=>{
-  overlay.classList.toggle("show");
-  originalContent.classList.toggle("show");
+  if(openOverlayBox.classList.contains("show")){
+    openOverlayBox.classList.remove("show");
+  }else{
+    openOverlayBox.classList.add("show");
+  }
 });
