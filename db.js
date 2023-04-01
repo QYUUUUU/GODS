77198c9bdc1edd9d339a5fe865affe7bcfbbf5f1
QYUUUUU <achoplin@gmail.com>
2023-04-01 15:36:06 +0200

    changing env variable

--- db.js
+++ db.js
@@ -10,10 +10,10 @@ require('dotenv').config();
 // Create a MySQL connection pool
 const pool = mysql.createPool({
   connectionLimit: 40,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'db',
+  user: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: 'godzila'
 });
 
 const leseletlepoivre = process.env.SALT;
